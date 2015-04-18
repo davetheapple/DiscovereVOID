1beba0837cdb6a75117fb32c2824e505d94b7675
davetheapple <davidbarrera@u.boisestate.edu>
2015-04-18 17:41:24 -0600

    main search

--- www/js/index.js
+++ www/js/index.js
@@ -45,16 +45,20 @@ $(document).ready(function() {
 	var $q = $('input[name="query"]');
 	//$q.css('visibility', 'hidden');
 	$('#search').on('click', function() {
-		
-		if($q.css('visibility') != 'visible') {
-			$q.css('visibility', 'visible');
-			$q.transition({width: '75%', opacity: '.85'}, 500, function() {
+		console.log($q.css('width'));
+		if(parseFloat($q.css('width')) <= 0) { //($q.css('visibility') != 'visible') {
+			/*$q.css('visibility', 'visible');
+			$q.transition({width: '75%', opacity: '.85'}, 1000, function() {
 				// none
-			});
-		} else if($q.css('visibility') == 'visible' && !$q.is(':focus')) {
-			$q.transition({width: '0', opacity: '0'}, 500, function() { 
+			});*/
+			$q.css({width: '75%', opacity: '.85'});
+		} else if(parseFloat($q.css('width')) >= 0 && !$q.is(':focus')) { // $q.css('visibility') == 'visible' &&
+			
+			$q.css({width: '0', opacity: '0'});
+			
+			/*$q.transition({width: '0', opacity: '0'}, 1000, function() { 
 				$q.css('visibility', 'hidden'); 
-			});
+			});*/
 		}
 	});
 	
